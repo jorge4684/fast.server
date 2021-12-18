4f5b600f78a2ff6891b7586a00f8772ce33a0573
jorge4684 <js5383651@gmail.com>
2021-12-18 01:42:04 -0600

    me falta la pasarela de pago en react

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRouter = require("./routes/user");
-
-
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -14,10 +18,16 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-  
-  app.use(express.json());
- app.use("/api/users", userRouter);
 
-app.listen(process.env.PORT || 5000, ()=> {
-   console.log("backend server is running!"); 
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
 });
